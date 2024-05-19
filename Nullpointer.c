95b8b941178f1bc778f3aadcd8457d714b5b115a
Syed Muhammad Omar Afnan <138956919+omar-afnan@users.noreply.github.com>
2024-05-19 08:31:21 +0400

    Add files via upload

---
+++ Nullpointer.c
@@ -0,0 +1,24 @@
+/*
+Write a program that initializes a pointer to NULL, 
+checks if the pointer is NULL, and prints an appropriate message.
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+
+ int *ptr=NULL;
+
+ if(ptr=NULL)
+ {
+    printf("pointer is null  ");
+ }
+ else
+ {
+    printf("The pointer is not NULL.\n");
+ }
+
+    return 0;
+}
